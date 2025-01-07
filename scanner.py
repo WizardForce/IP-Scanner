5566c822bb80679e4cdefd17dec8e92f35775a31
WizardForce <hannamark820@gmail.com>
2025-01-07 18:02:51 -0500

    High Seas

--- scanner.py
+++ scanner.py
@@ -1,39 +1,34 @@
 import socket
-#IPs
+# IPs
 start_ip = "192.168.0.1"
 end_ip = "192.168.0.255"
-#Into octets
+# Into octets
 start_octet = start_ip.split(".")
 end_octet = end_ip.split(".")
-#conversion to integers
+# Conversion to integers
 start_octet = [int(octet) for octet in start_octet]
-start_octets[3] += 1
+start_octet[3] += 1
 end_octet = [int(octet) for octet in end_octet]
-#initialize
-current_octet = start_octets[:]
-#socket object
+# Initialize
+current_octet = start_octet[:]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#timeout
+# Timeout
 sock.settimeout(0.5)
-#Scan IPs
+# Scan IPs
 while current_octet != end_octet:
-
-    #Construct the IP
+    # Construct the IP
     current_ip = ".".join([str(octet) for octet in current_octet])
-    #Try to connect
+    # Try to connect
     result = sock.connect_ex((current_ip, 80))
-    #If success
+    # If success
     if result == 0:
         print(f"IP {current_ip} is up")
-
-    #Increment the current IP
+    # Increment the current IP
     current_octet[3] += 1
-
-    #if the current octet is 255
+    # If the current octet is 255
     for i in range(3, 0, -1):
         if current_octet[i] == 256:
             current_octet[i] = 0
             current_octet[i-1] += 1
-        
-#close socket
-s.close()
+# Close socket
+sock.close()
